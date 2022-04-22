807dffe4f40f7a971afac378d60040f8cc504646
Dewey Dunnington <dewey@fishandwhistle.net>
2022-04-22 16:14:20 -0300

    move into a subdir

---
+++ src/s2geography.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "s2geography/geography.hpp"
+#include "s2geography/index.hpp"
+#include "s2geography/accessors.hpp"
+#include "s2geography/accessors-geog.hpp"
+#include "s2geography/linear-referencing.hpp"
+#include "s2geography/distance.hpp"
+#include "s2geography/build.hpp"
+#include "s2geography/coverings.hpp"
+#include "s2geography/predicates.hpp"
+#include "s2geography/constructor.hpp"
