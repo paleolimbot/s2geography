3e8058632642f6bd67c9fe11d47db0d6dca93e6c
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-17 15:12:48 -0400

    maybe better vendoring setup

--- src/s2geography/geoarrow_test.cc
+++ src/s2geography/geoarrow_test.cc
@@ -3,11 +3,6 @@
 
 #include "s2geography.h"
 
-namespace s2geography {
-
-const char* s2_geoarrow_version();
-
-}
 
 TEST(GeoArrow, GeoArrowVersionTest) {
   EXPECT_STREQ(s2geography::s2_geoarrow_version(), "0.2.0-SNAPSHOT");
