052f80740a534cc4d83e5bd9df39565f951f908a
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-17 22:45:08 -0400

    setup reader

--- src/s2geography/geoarrow_test.cc
+++ src/s2geography/geoarrow_test.cc
@@ -3,6 +3,16 @@
 
 #include "s2geography.h"
 
+using s2geography::geoarrow::Reader;
+
 TEST(GeoArrow, GeoArrowVersionTest) {
   EXPECT_STREQ(s2geography::geoarrow::version(), "0.2.0-SNAPSHOT");
+}
+
+TEST(GeoArrow, GeoArrowReaderBasic) {
+  Reader reader;
+  ArrowSchema schema;
+  schema.release = nullptr;
+
+  EXPECT_THROW(reader.Init(&schema), s2geography::Exception);
 }
