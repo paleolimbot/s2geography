7bac43c929c7283ca765d69bb6521b23efe70b7d
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-19 04:20:51 +0000

    fix: Update build configuration so that a DuckDB Extension can be built (#32)

--- src/vendored/geoarrow/double_parse_fast_float.cc
+++ src/vendored/geoarrow/double_parse_fast_float.cc
@@ -5,9 +5,14 @@
 
 #include "fast_float/fast_float.h"
 
+// When building a DuckDB extension, this is duckdb_fast_float
+#if !defined(GEOARROW_FAST_FLOAT_NAMESPACE)
+#define GEOARROW_FAST_FLOAT_NAMESPACE fast_float
+#endif
+
 extern "C" GeoArrowErrorCode GeoArrowFromChars(const char* first, const char* last,
                                                double* out) {
-  auto answer = fast_float::from_chars(first, last, *out);
+  auto answer = GEOARROW_FAST_FLOAT_NAMESPACE::from_chars(first, last, *out);
   if (answer.ec != std::errc()) {
     return EINVAL;
   } else {
