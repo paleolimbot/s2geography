5b68532558fd0924d3d9e4555e41678e685316c5
Dewey Dunnington <dewey@fishandwhistle.net>
2024-11-21 22:08:37 -0600

    clang-format on projections

--- src/s2geography/projections.cc
+++ src/s2geography/projections.cc
@@ -1,29 +1,32 @@
 
-#include "s2/s2projections.h"
-
 #include <s2/r2.h>
 #include <s2/s2latlng.h>
 #include <s2/s2point.h>
 #include <s2/s2pointutil.h>
 
+#include "s2/s1chord_angle.h"
+#include "s2/s2projections.h"
+
 namespace s2geography {
 
 std::shared_ptr<S2::Projection> lnglat() {
-  std::shared_ptr<S2::Projection> projection = std::make_shared<S2::PlateCarreeProjection>(180);
+  std::shared_ptr<S2::Projection> projection =
+      std::make_shared<S2::PlateCarreeProjection>(180);
   return std::move(projection);
 }
 
 std::shared_ptr<S2::Projection> pseudo_mercator() {
   // the semi-major axis of the WGS 84 ellipsoid is 6378137 meters
-  // -> half of the circumference of the sphere is PI * 6378137 = 20037508.3427892
-  std::shared_ptr<S2::Projection> projection = std::make_shared<S2::MercatorProjection>(20037508.3427892);
+  // -> half of the circumference of the sphere is PI * 6378137 =
+  // 20037508.3427892
+  std::shared_ptr<S2::Projection> projection =
+      std::make_shared<S2::MercatorProjection>(20037508.3427892);
   return std::move(projection);
 }
 
-class OrthographicProjection: public S2::Projection {
-public:
-  OrthographicProjection(const S2LatLng& centre):
-      centre_(centre) {
+class OrthographicProjection : public S2::Projection {
+ public:
+  OrthographicProjection(const S2LatLng& centre) : centre_(centre) {
     z_axis_ = S2Point(0, 0, 1);
     y_axis_ = S2Point(0, 1, 0);
   }
@@ -46,24 +49,21 @@ public:
     return out;
   }
 
-  R2Point FromLatLng(const S2LatLng& ll) const {
-    return Project(ll.ToPoint());
-  }
+  R2Point FromLatLng(const S2LatLng& ll) const { return Project(ll.ToPoint()); }
 
-  S2LatLng ToLatLng(const R2Point& p) const {
-    return S2LatLng(Unproject(p));
-  }
+  S2LatLng ToLatLng(const R2Point& p) const { return S2LatLng(Unproject(p)); }
 
-  R2Point wrap_distance() const {return R2Point(0, 0); }
+  R2Point wrap_distance() const { return R2Point(0, 0); }
 
-private:
+ private:
   S2LatLng centre_;
   S2Point z_axis_;
   S2Point y_axis_;
 };
 
 std::shared_ptr<S2::Projection> orthographic(const S2LatLng& centre) {
-  std::shared_ptr<S2::Projection> projection = std::make_shared<OrthographicProjection>(centre);
+  std::shared_ptr<S2::Projection> projection =
+      std::make_shared<OrthographicProjection>(centre);
   return std::move(projection);
 }
 
