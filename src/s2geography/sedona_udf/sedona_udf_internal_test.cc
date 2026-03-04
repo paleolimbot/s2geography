f43531e2097e15dfe4ae27114cb02f2904d2ce77
Dewey Dunnington <dewey@wherobots.com>
2026-03-03 21:12:40 -0600

    remove unnecessary tests

--- src/s2geography/sedona_udf/sedona_udf_internal_test.cc
+++ src/s2geography/sedona_udf/sedona_udf_internal_test.cc
@@ -3,7 +3,7 @@
 #include "geoarrow/geoarrow.hpp"
 #include "nanoarrow/nanoarrow.hpp"
 #include "s2geography/accessors-geog.h"
-#include "s2geography/sedona_udf/sedona_udf_test_internal.h"
+#include "s2geography/linear-referencing.h"
 
 // Tests the matching of the Arrow argument and also propagation of the CRS from
 // the first argument in a binary kernel.
@@ -86,85 +86,4 @@ TEST(SedonaUdf, GeographyInputMatches) {
 
   impl.release(&impl);
   kernel.release(&kernel);
-}
-
-TEST(SedonaUdf, GeographyToArrow) {
-  struct SedonaCScalarKernel kernel;
-  s2geography::sedona_udf::LengthKernel(&kernel);
-  struct SedonaCScalarKernelImpl impl;
-  ASSERT_NO_FATAL_FAILURE(
-      TestInitKernel(&kernel, &impl, {ARROW_TYPE_WKB}, NANOARROW_TYPE_DOUBLE));
-
-  nanoarrow::UniqueArray out_array;
-  ASSERT_NO_FATAL_FAILURE(
-      TestExecuteKernel(&impl, {ARROW_TYPE_WKB}, NANOARROW_TYPE_DOUBLE,
-                        {{"POINT (0 1)", "LINESTRING (0 0, 0 1)",
-                          "POLYGON ((0 0, 0 1, 1 0, 0 0))", std::nullopt}},
-                        {}, out_array.get()));
-  impl.release(&impl);
-  kernel.release(&kernel);
-
-  ASSERT_NO_FATAL_FAILURE(
-      TestResultArrow(out_array.get(), NANOARROW_TYPE_DOUBLE,
-                      {0.0, 111195.10117748393, 0.0, std::nullopt}));
-}
-
-TEST(SedonaUdf, GeographyToGeography) {
-  struct SedonaCScalarKernel kernel;
-  s2geography::sedona_udf::CentroidKernel(&kernel);
-  struct SedonaCScalarKernelImpl impl;
-  ASSERT_NO_FATAL_FAILURE(
-      TestInitKernel(&kernel, &impl, {ARROW_TYPE_WKB}, ARROW_TYPE_WKB));
-
-  nanoarrow::UniqueArray out_array;
-  ASSERT_NO_FATAL_FAILURE(
-      TestExecuteKernel(&impl, {ARROW_TYPE_WKB}, NANOARROW_TYPE_DOUBLE,
-                        {{"POINT (0 1)", "LINESTRING (0 0, 0 1)",
-                          "POLYGON ((0 0, 0 1, 1 0, 0 0))", std::nullopt}},
-                        {}, out_array.get()));
-  impl.release(&impl);
-  kernel.release(&kernel);
-
-  ASSERT_NO_FATAL_FAILURE(TestResultGeography(
-      out_array.get(), {"POINT (0 1)", "POINT (0 0.5)",
-                        "POINT (0.33335 0.333344)", std::nullopt}));
-}
-
-TEST(SedonaUdf, GeographyGeographyToGeography) {
-  struct SedonaCScalarKernel kernel;
-  s2geography::sedona_udf::ClosestPointKernel(&kernel);
-  struct SedonaCScalarKernelImpl impl;
-  ASSERT_NO_FATAL_FAILURE(TestInitKernel(
-      &kernel, &impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB}, ARROW_TYPE_WKB));
-
-  nanoarrow::UniqueArray out_array;
-  ASSERT_NO_FATAL_FAILURE(TestExecuteKernel(
-      &impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB}, ARROW_TYPE_WKB,
-      {{"POINT (0 1)", "LINESTRING (0 0, 0 1)", std::nullopt}, {"POINT (0 0)"}},
-      {}, out_array.get()));
-  impl.release(&impl);
-  kernel.release(&kernel);
-
-  ASSERT_NO_FATAL_FAILURE(TestResultGeography(
-      out_array.get(), {"POINT (0 1)", "POINT (0 0)", std::nullopt}));
-}
-
-TEST(SedonaUdf, GeographyArrowToGeography) {
-  struct SedonaCScalarKernel kernel;
-  s2geography::sedona_udf::LineInterpolatePointKernel(&kernel);
-  struct SedonaCScalarKernelImpl impl;
-  ASSERT_NO_FATAL_FAILURE(TestInitKernel(
-      &kernel, &impl, {ARROW_TYPE_WKB, NANOARROW_TYPE_DOUBLE}, ARROW_TYPE_WKB));
-
-  nanoarrow::UniqueArray out_array;
-  ASSERT_NO_FATAL_FAILURE(
-      TestExecuteKernel(&impl, {ARROW_TYPE_WKB, NANOARROW_TYPE_DOUBLE},
-                        ARROW_TYPE_WKB, {{"LINESTRING (0 0, 0 1)"}},
-                        {{0.0, 0.5, 1.0, std::nullopt}}, out_array.get()));
-  impl.release(&impl);
-  kernel.release(&kernel);
-
-  ASSERT_NO_FATAL_FAILURE(TestResultGeography(
-      out_array.get(),
-      {"POINT (0 0)", "POINT (0 0.5)", "POINT (0 1)", std::nullopt}));
 }
