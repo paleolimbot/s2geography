929adfadbf387c6ca86f5c3afb3d49e3fdfc95bd
Dewey Dunnington <dewey@wherobots.com>
2026-03-11 14:59:26 -0500

    add one test

--- src/s2geography/predicates_test.cc
+++ src/s2geography/predicates_test.cc
@@ -5,7 +5,7 @@
 #include "nanoarrow/nanoarrow.hpp"
 #include "s2geography/sedona_udf/sedona_udf_test_internal.h"
 
-TEST(Predicates, SedonaUdfIntersects) {
+TEST(Predicates, SedonaUdfIntersectsScalarArray) {
   struct SedonaCScalarKernel kernel;
   s2geography::sedona_udf::IntersectsKernel(&kernel);
   struct SedonaCScalarKernelImpl impl;
@@ -25,7 +25,27 @@ TEST(Predicates, SedonaUdfIntersects) {
                                           {true, false, std::nullopt}));
 }
 
-TEST(Predicates, SedonaUdfEquals) {
+TEST(Predicates, SedonaUdfIntersectsArrayScalar) {
+  struct SedonaCScalarKernel kernel;
+  s2geography::sedona_udf::IntersectsKernel(&kernel);
+  struct SedonaCScalarKernelImpl impl;
+  ASSERT_NO_FATAL_FAILURE(TestInitKernel(
+      &kernel, &impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB}, NANOARROW_TYPE_BOOL));
+
+  nanoarrow::UniqueArray out_array;
+  ASSERT_NO_FATAL_FAILURE(
+      TestExecuteKernel(&impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB},
+                        {{"POINT (0.25 0.25)", "POINT (-1 -1)", std::nullopt},
+                         {"POLYGON ((0 0, 1 0, 0 1, 0 0))"}},
+                        {}, out_array.get()));
+  impl.release(&impl);
+  kernel.release(&kernel);
+
+  ASSERT_NO_FATAL_FAILURE(TestResultArrow(out_array.get(), NANOARROW_TYPE_BOOL,
+                                          {true, false, std::nullopt}));
+}
+
+TEST(Predicates, SedonaUdfEqualsScalarArray) {
   struct SedonaCScalarKernel kernel;
   s2geography::sedona_udf::EqualsKernel(&kernel);
   struct SedonaCScalarKernelImpl impl;
@@ -46,7 +66,7 @@ TEST(Predicates, SedonaUdfEquals) {
                                           {true, false, std::nullopt}));
 }
 
-TEST(Predicates, SedonaUdfContains) {
+TEST(Predicates, SedonaUdfContainsScalarArray) {
   struct SedonaCScalarKernel kernel;
   s2geography::sedona_udf::ContainsKernel(&kernel);
   struct SedonaCScalarKernelImpl impl;
@@ -64,4 +84,33 @@ TEST(Predicates, SedonaUdfContains) {
 
   ASSERT_NO_FATAL_FAILURE(TestResultArrow(out_array.get(), NANOARROW_TYPE_BOOL,
                                           {true, false, std::nullopt}));
+}
+
+TEST(Predicates, SedonaUdfScalarScalar) {
+  std::optional<std::string> lhs = "POLYGON ((0 0, 2 0, 0 2, 0 0))";
+  std::string op = "intersects";
+  std::optional<std::string> rhs = "POINT (0.25 0.25)";
+  std::optional<bool> result = true;
+
+  struct SedonaCScalarKernel kernel;
+  struct SedonaCScalarKernelImpl impl;
+  if (op == "intersects") {
+    s2geography::sedona_udf::IntersectsKernel(&kernel);
+
+  } else {
+    ASSERT_TRUE(false) << "Unknown predicate";
+  }
+
+  ASSERT_NO_FATAL_FAILURE(TestInitKernel(
+      &kernel, &impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB}, NANOARROW_TYPE_BOOL));
+
+  nanoarrow::UniqueArray out_array;
+  ASSERT_NO_FATAL_FAILURE(
+      TestExecuteKernel(&impl, {ARROW_TYPE_WKB, ARROW_TYPE_WKB}, {{lhs}, {rhs}},
+                        {}, out_array.get()));
+  impl.release(&impl);
+  kernel.release(&kernel);
+
+  ASSERT_NO_FATAL_FAILURE(
+      TestResultArrow(out_array.get(), NANOARROW_TYPE_BOOL, {result}));
 }
